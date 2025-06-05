45fad8a23c396ee694e97aa34d9cf76aa40064c0
PashaBorzenkov <p.borzenkov@purpleplane-it.com>
2025-06-05 22:46:30 +0300

    rm useless

---
+++ frontend/src/app/providers/router/index.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routerConfig } from '../../../../config/router';
+
+export const AppRouter = () => (
+    <Suspense fallback="">
+        <Routes>
+            {
+                Object.values(routerConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        element={(
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        )}
+                        path={path}
+                    />
+                ))
+            }
+        </Routes>
+    </Suspense>
+);
